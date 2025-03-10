98b06ac80521835734296872d4c61a98805f7e67
Pavel Sofronov <sofronovpv89@mail.ru>
2025-03-10 11:43:46 +0400

    delete excess comments

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,40 +1,5 @@
-import _ from 'lodash';
 import getPath from '../getPath.js';
 
-// const formatValue = (value) => {
-//   if (_.isNull(value) || _.isBoolean(value) || _.isNumber(value)) {
-//     return value;
-//   } if (_.isPlainObject(value)) {
-//     return JSON.stringify(value);
-//   }
-//   return `"${value}"`;
-// };
-
-// const getValue = (elem) => {
-//   if (elem.keyOld || elem.keyNew) {
-//     return `"oldValue": ${formatValue(elem.keyOld)}, "newValue": ${formatValue(
-//       elem.keyNew,
-//     )}`;
-//   }
-//   return `"value": ${formatValue(elem.value)}`;
-// };
-
-// const formatJson = (data, parentPath = '') => {
-//   const iner = (items, path) => items
-//     .filter((elem) => elem.condition !== 'dash')
-//     .map((elem) => {
-//       const { condition, children } = elem;
-//       const currentPath = getPath(elem, path);
-//       if (condition === 'unchanged') {
-//         return `${iner(children, currentPath)}`;
-//       }
-//       return `{"name": "${currentPath}", ${getValue(
-//         elem,
-//       )}, "condition": "${condition}"}`;
-//     });
-//   const result = iner(data, parentPath).join(',');
-//   return `[${result}]`;
-// };
 const formatJson = (data, parentPath = '') => {
   const iner = (items, path) => items
     .filter((elem) => elem.condition !== 'dash')
